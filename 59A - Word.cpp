2bfdcbdd5da74e353f5f65b268ce2bd52f5dfd79
S M Shakib Hossain <smsakib2403@gmail.com>
2021-02-22 22:50:02 +0600

    Create 59A - Word.cpp

---
+++ 59A - Word.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+    string s;
+    cin >> s;
+    int upper = 0, lower = 0;
+    for (int i = 0; i < s.length(); ++i)
+    {
+        if (isupper(s[i]))
+        {
+            upper += 1;
+        }
+        else
+        {
+            lower += 1;
+        }
+    }
+    if (upper > lower)
+    {
+        for (int i = 0; i < s.length(); ++i)
+        {
+            s[i] = toupper(s[i]);
+        }
+    }
+    else
+    {
+        for (int i = 0; i < s.length(); ++i)
+        {
+            s[i] = tolower(s[i]);
+        }
+    }
+    cout << s << endl;
+    return 0;
+}
