db8d6720582adbb7760d2c0ff483c4304818d3b5
S M Shakib Hossain <smsakib2403@gmail.com>
2021-02-28 13:16:45 +0600

    Create 379A - New Year Candles.cpp

---
+++ 379A - New Year Candles.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a,b,d,m;
+    cin>>a>>b;
+    int h=a;
+
+    while(a>=b)
+    {
+        d=a/b;
+        h+=d;
+        m=a%b;
+        a=d+m;}
+    cout<<h <<endl;
+    return 0;
+}
