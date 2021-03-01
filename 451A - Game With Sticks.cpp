e178c1bfd980daeac202e5f99d3eb1f35db9ad9d
S M Shakib Hossain <smsakib2403@gmail.com>
2021-03-01 21:28:56 +0600

    Create 451A - Game With Sticks.cpp

---
+++ 451A - Game With Sticks.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int a,b;
+    cin>>a>>b;
+    a=min(a,b);
+    if(a%2==0){
+        cout<<"Malvika\n";}
+    else
+        cout<<"Akshat\n";
+    return 0;
+    
+}
